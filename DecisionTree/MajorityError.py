08fa5daa4c0f700b9a661111d6ecf2283bcc032f
Ryan Lam <ryanjlam@hotmail.com>
2022-09-23 02:38:48 -0600

    Bug on CADE machine, test version

--- DecisionTree/MajorityError.py
+++ DecisionTree/MajorityError.py
@@ -70,6 +70,7 @@ class ME(object):
             
             for label in vals.index.levels[0]:
                 cat = vals.loc[label,:]
+                print(cat)
                 h = 1-cat[0]/sum(cat)
                 ent += h * sum(cat)/sum(vals)
             g = initialEntropy - ent
